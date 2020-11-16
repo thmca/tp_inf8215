26f0eb98671e26ba79c1a50be28413cb09f3769e
Emmanuelle Roberge <emmanuelle.roberge@polymtl.ca>
2020-11-16 14:41:02 -0500

    ER : Starting tensorflow

--- tp3/main.py
+++ tp3/main.py
@@ -1,1 +1,24 @@
-print("Hello world")
+import nltk
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+
+
+data_file = open("data/train.csv", "r")
+test_file = open("data/test.csv", "r")
+submission_file = open("data/submission.csv", "w")
+
+df=pd.read_csv(data_file)
+
+df.head()
+
+#True = phishing
+df['phishing']=pd.get_dummies(df['status'], drop_first=True)
+
+df=df.drop('status', axis=1)
+
+df.info()
+print(df)
+
