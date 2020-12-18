d0f6ab668037402a25f770c57d38ff48f9c619ee
SamuelA-L <sam.aglussier@gmail.com>
2020-12-17 19:02:37 -0500

    SAL : bug fixed for pca on test set new submissioon

--- tp3/main.py
+++ tp3/main.py
@@ -13,12 +13,34 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.preprocessing import label_binarize
 from math import floor
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import label_binarize, StandardScaler
 
 import model1
 import model2
 from ScikitModel import predict
 import baynes
 import randomForest
+
+def normal_scaling(train_set, validation_set, test_set):
+    scaler = StandardScaler()
+    scaler.fit(train_set)
+    new_train = scaler.transform(train_set)
+    new_validate = scaler.transform(validation_set)
+    new_test = scaler.transform(test_set)
+
+    return new_train, new_validate, new_test
+
+
+def apply_PCA(train_set, validation_set, test_set):
+    train_scaled, validate_scaled, test_scaled  = normal_scaling(train_set, validation_set, test_set)
+    pca = PCA(n_components='mle')
+    pca.fit(train_scaled)
+    new_train = pca.transform(train_scaled)
+    new_validate = pca.transform(validate_scaled)
+    new_test = pca.transform(test_scaled)
+
+    return new_train, new_validate, new_test
 
 def mean_model_calculator(predictions):
     new_frame = pd.concat(predictions, axis=1, sort=False)
@@ -52,8 +74,7 @@ data_df["domain_age"] = data_df["domain_age"].replace(np.nan, domain_age_mean)
 
 # This can be used when validation is needed
 train_df, validate_df = train_test_split(data_df, test_size=0.2)
-print("train : ", train_df["status"].value_counts())
-print("validate : ", validate_df["status"].value_counts())
+
 
 # This is used when we want to use the entire train csv for submission
 y_all = data_df.iloc[:, (data_df.shape[1] - 1)]
@@ -66,10 +87,9 @@ y_validate = validate_df.iloc[:, (train_df.shape[1] - 1)]
 x_validate = validate_df.iloc[:, 1:(train_df.shape[1] - 1)]
 
 # use pca
-x_train_PCA, x_validate_PCA, test_df_PCA = model1.apply_PCA(x_train, x_validate, test_df)
+x_train_PCA, x_validate_PCA, test_df_PCA = apply_PCA(x_train, x_validate, test_df)
 
 prediction_model1, dnn1 = model1.validate_predictions(x_train_PCA, x_validate_PCA, y_train)
-# prediction_model2, dnn2 = model2.validate_predictions(x_train_PCA, x_validate_PCA, y_train)
 prediction_model3, rf_model = randomForest.validate_predictions(x_train, y_train, x_validate, y_validate)
 
 prediction_ceiling = 0.55
@@ -77,15 +97,10 @@ prediction_ceiling = 0.55
 predictions = mean_model_calculator([prediction_model1, prediction_model3])
 
 binary_prediction_model1_df = prediction_model1 >= prediction_ceiling
-# binary_prediction_model2_df = prediction_model2 >= prediction_ceiling
 binary_predictions = predictions >= prediction_ceiling
 
 print("deep learning model f1 score ", " : ",
           f1_score(y_validate, binary_prediction_model1_df))
-
-
-# print("deep learning model f1 score ", " : ",
-#           f1_score(y_validate, binary_prediction_model2_df))
 
 
 print("combined model f1 score ", " : ",
@@ -95,9 +110,6 @@ print("combined model f1 score ", " : ",
 predictions1 = dnn1.predict(test_df_PCA)
 predictions1_df = pd.DataFrame(data=predictions1)
 
-# predictions2 = dnn2.predict(test_df_PCA)
-# predictions2_df = pd.DataFrame(data=predictions2)
-
 predictions3_df = predict(rf_model, test_df)
 
 predictions_mean = mean_model_calculator([predictions1_df, predictions3_df])
@@ -106,8 +118,13 @@ binary_test_prediction = predictions_mean >= prediction_ceiling
 
 predictions1_df = predictions1_df >= prediction_ceiling
 
+
 print(predictions1_df.shape)
 submit(predictions1_df, "DNN")
+submit(predictions3_df, "RF")
+submit(binary_test_prediction, "RF_DNN")
 
-
+print("Data \n ", data_df["status"].value_counts())
+print("train \n ", train_df["status"].value_counts())
+print("validate \n ", validate_df["status"].value_counts())
 
--- tp3/model1.py
+++ tp3/model1.py
@@ -1,26 +1,5 @@
 import pandas as pd
-from sklearn.preprocessing import label_binarize, StandardScaler
-from sklearn.decomposition import PCA
 from tensorflow import keras
-
-def normal_scaling(train_set, validation_set):
-    scaler = StandardScaler()
-    scaler.fit(train_set)
-    new_train = scaler.transform(train_set)
-    new_validate = scaler.transform(validation_set)
-
-    return new_train, new_validate
-
-
-def apply_PCA(train_set, validation_set, test_set):
-    train_scaled, validate_scaled = normal_scaling(train_set, validation_set)
-    pca = PCA(n_components='mle')
-    pca.fit(train_scaled)
-    new_train = pca.transform(train_scaled)
-    new_validate = pca.transform(validate_scaled)
-    new_test = pca.transform(test_set)
-
-    return new_train, new_validate, new_test
 
 
 def validate_predictions(x_train, x_validate, y_train):
@@ -35,18 +14,19 @@ def validate_predictions(x_train, x_validate, y_train):
     deep_model = keras.Sequential()
     deep_model.add(keras.Input(shape=(n_features,)))
     deep_model.add(keras.layers.Dense(16, activation='relu'))
-    deep_model.add(keras.layers.Dropout(0.2))
-    deep_model.add(keras.layers.Dense(16, activation='relu'))
-    deep_model.add(keras.layers.Dropout(0.2))
-    deep_model.add(keras.layers.Dense(16, activation='relu'))
-    deep_model.add(keras.layers.Dropout(0.2))
+    deep_model.add(keras.layers.Dropout(0.1))
+    deep_model.add(keras.layers.Dense(32, activation='relu'))
+    deep_model.add(keras.layers.Dropout(0.1))
+    deep_model.add(keras.layers.Dense(32, activation='relu'))
+    deep_model.add(keras.layers.Dropout(0.1))
+    deep_model.add(keras.layers.Dense(16, activation='tanh'))
+    deep_model.add(keras.layers.Dropout(0.1))
     deep_model.add(keras.layers.Dense(1, activation='sigmoid'))
     deep_model.summary()
 
     # Model parameters
-    epochs = 150
+    epochs = 200
     batch_size = 64
-    classification_ceiling = 0.6
     optimizer = keras.optimizers.Adam(learning_rate=0.002)  # default 0.001 Sets the learning rate for the code bellow
 
     deep_model.compile(
@@ -67,49 +47,3 @@ def validate_predictions(x_train, x_validate, y_train):
 
     return prediction_df, deep_model
 
-def submission_predictions(x_all,y_all, test_df):
-
-    #This is for the submission only. When un-commenting comment  code between stars
-    x_all_PCA, test_PCA = apply_PCA(x_all, test_df)
-
-    n_features = x_all_PCA.shape[1]
-
-    # Model structure
-    # possible activations : tanh, relu, elu, selu
-    # for explanations on activation functions see :
-    # https://subscription.packtpub.com/book/programming/9781838821654/1/ch01lvl1sec04/3-multilayer-perceptron-mlp
-
-    deep_model2 = keras.Sequential()
-    deep_model2.add(keras.Input(shape=(n_features,)))
-    deep_model2.add(keras.layers.Dense(16, activation='selu'))
-    deep_model2.add(keras.layers.Dropout(0.1))
-    deep_model2.add(keras.layers.Dense(16, activation='relu'))
-    deep_model2.add(keras.layers.Dropout(0.1))
-    deep_model2.add(keras.layers.Dense(16, activation='tanh'))
-    deep_model2.add(keras.layers.Dropout(0.1))
-    deep_model2.add(keras.layers.Dense(1, activation='sigmoid'))
-    deep_model2.summary()
-
-    # Model parameters
-    epochs = 150
-    batch_size = 128
-    classification_ceiling = 0.6
-    optimizer = keras.optimizers.Adam(learning_rate=0.002)  # default 0.001 Sets the learning rate for the code bellow
-
-    deep_model2.compile(
-        optimizer=optimizer,
-        loss='binary_crossentropy',
-        metrics=['accuracy']
-    )
-
-    # comment fit and save models lines and uncomment load line to load last saved model without having to retrain
-    deep_model2.fit(x_all_PCA, y_all, epochs=epochs, batch_size=batch_size)
-    deep_model2.save("models/model1")
-    # deep_model2 = keras.models.load_model("models/model1")
-
-
-    deep_predictions = deep_model2.predict(test_PCA)
-    deep_predictions = deep_predictions > classification_ceiling
-    deep_prediction_df = pd.DataFrame(data=deep_predictions)
-
-    return deep_prediction_df
